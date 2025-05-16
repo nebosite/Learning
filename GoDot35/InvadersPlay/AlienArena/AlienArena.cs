7fa3c72f05879d1da44812e68de2a771c1c32e95
Eric Jorgensen <ericjorg@thejcrew.net>
2025-05-16 16:39:49 -0700

    Refactored with better understanding of scenes

--- GoDot35/InvadersPlay/AlienArena/AlienArena.cs
+++ GoDot35/InvadersPlay/AlienArena/AlienArena.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class TestScene : KinematicBody2D
+public class AlienArena : KinematicBody2D
 {
     // Declare member variables here. Examples:
     // private int a = 2;
---
+++ GoDot35/InvadersPlay/Pages/Earth.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public class Earth : KinematicBody2D
+{
+    private PackedScene playerSource;
+    private PackedScene alienSource;
+    private Timer _spawnTimer;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _spawnTimer = new Timer();
+        _spawnTimer.WaitTime = 2.0f;
+        _spawnTimer.OneShot = false;
+        _spawnTimer.Autostart = true;
+        AddChild(_spawnTimer);
+        _spawnTimer.Connect("timeout", this, nameof(OnSpawnTimeout));
+
+        playerSource = GD.Load<PackedScene>("res://Components/Players/PlayerTypeA.tscn");
+        var player = (KinematicBody2D)playerSource.Instance();
+        AddChild(player);
+        player.Position = new Vector2(200, 200);
+
+        alienSource = GD.Load<PackedScene>("res://Components/Aliens/Alien1.tscn");
+    }
+
+    private void OnSpawnTimeout()
+    {
+        SpawnAlien();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+    }
+    
+     private void SpawnAlien()
+    {
+        // 5. Instance the Alien scene
+        var alienInstance = (Node2D)alienSource.Instance();
+
+        // 6. Position it somewhere (e.g., random x at top of screen)
+        var screenSize = GetViewportRect().Size;
+        float x = (float)GD.RandRange(100, screenSize.x - 100);
+        alienInstance.Position = new Vector2(x, 150); // just above the top
+
+        // 7. Add it to the scene tree (as a sibling of the Spawner)
+        GetParent().AddChild(alienInstance);
+    }
+}
